e37905da2b1f39160f7462d26754281c11e9b9f6
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2022-05-20 16:29:39 -0500

    Proyecto inicial

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
   },
   getters: {
   },
   mutations: {
+    setcharacters(state, payload){
+      state.characters = payload
+    },
+    setcharactersFilter(state, payload){
+      state.characters = payload
+    }
   },
   actions: {
+    async getcharacters({commit}){
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        consoleg.log(data)
+      } catch (error) {
+        consoleg.error(error)
+      }
+    }
   },
   modules: {
   }
